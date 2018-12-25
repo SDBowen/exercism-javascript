a741d1d91128c6e2cbf36b80ba11fe7c971a58b9
Steven Bowen <sdbowen.git@gmail.com>
2018-12-25 08:06:40 -0600

    Add two-fer exercise and gitignore

---
+++ two-fer/two-fer.js
@@ -0,0 +1,12 @@
+var TwoFer = function() {};
+
+TwoFer.prototype.twoFer = function(who) {
+  // If no name was passed in, set who to 'you'
+  if (!who) {
+    who = 'you';
+  }
+
+  return `One for ${who}, one for me.`;
+};
+
+module.exports = TwoFer;
